51740997a237beeae2ae2c8d97f95377b635e1c7
Mohammad Amin Rezaei Sepehr <mohammadaminrez@gmail.com>
2025-05-01 10:58:48 +0200

    refactor: rename app to foody and update application identifiers

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.foody_ai_app"
+    namespace = "io.github.mohammadaminrez.foody"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.foody_ai_app"
+        applicationId = "io.github.mohammadaminrez.foody"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
