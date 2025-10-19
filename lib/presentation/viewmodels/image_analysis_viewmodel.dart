b730756b2793aa264de7936961100a6000d59748
Mohsen Kashefi <mohsenkashefi2016@yahoo.com>
2025-10-18 19:30:10 -0700

    enahncement : exceprion  handling

--- lib/presentation/viewmodels/image_analysis_viewmodel.dart
+++ lib/presentation/viewmodels/image_analysis_viewmodel.dart
@@ -15,6 +15,7 @@ import '../../services/sync_service.dart';
 import '../../services/aws_service.dart';
 import '../../services/permission_service.dart';
 import '../../core/events/food_data_update_event.dart';
+import '../../core/services/connection_service.dart';
 import '../widgets/rating_dialog.dart';
 import '../../config/routes/navigation_service.dart';
 
